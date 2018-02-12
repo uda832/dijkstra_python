cc9a67b46abb19ba22d95f50a3360074b2d01ba7
O Yeruultsengel <oyeruultsengel@etelligentsolutions.com>
2018-02-11 19:44:20 -0500

    re-implement, lazily sort the heap at each iteration to emulate a 'min-queue'

--- ShortestPath.py
+++ ShortestPath.py
@@ -1,3 +1,8 @@
+# Author: Uda Yeruultsengel
+# Dijkstra Solver
+# 02/11/2018
+
+import time
 import heapq
 import math
 
@@ -7,6 +12,9 @@ class ShortestPathSolver:
         self.start = start
         self.goal = goal
         self.table = table
+        self.heap = []
+        self.shortestPath = []
+        self.nodes = None
 
     def find_shortest_path(self):
         print("Starting vertex: {}".format(self.start))
@@ -17,139 +25,152 @@ class ShortestPathSolver:
             Structure of Node: (idOfNode, costFromStart, idOfParent)
         '''
 
-        self.shortestPath = []
-        heap = []               # Initialize priority queue (i.e. min-heap)
-        closedDict = {}         # Initilize the closed dict (i.e. discovered nodes)
-        distances = {}
+        # print("DEBUG: curNode = {} and grab_edges(curNode) = {}".format(curNode, self.grab_edges(curNode)))
 
-        # Insert the start node
-        startNode = (self.start, 0, -99)                  # Note structure of node ( nodeId,costFromStart, parentId)
-        # heapq.heappush(heap, startNode)
-        heap.append(startNode)
-        heap.sort(reverse=True, key=lambda x: x[1])
-        
 
-        # While the heap is not empty
-        while heap:
-            heap.sort(reverse=True, key=lambda x: x[1])
-            # curNode = heapq.heappop(heap)
-            curNode = heap.pop()
 
-            print("DEBUG: current minimum node -- {}".format(curNode))
-            print("\t\tDEBUG: current contents of heap -- {}".format(heap))
-            print("\t\tDEBUG: current contents of closedDict -- {}".format(closedDict))
+        self.nodes = self.build_vertices_list()
+        self.heap = []       # list of nodes (where node is a tuple in the following format: (id, idOfParent)
+        distances = {x: math.inf for x in self.nodes}  # Dict of distances where the id of the node is used as key
+        closedDict = {}
+        goalNode = None
 
-            pathCost = curNode[1]                   # Distance to current node from the start node
-            closedDict[curNode[0]] = curNode        # Insert into closedDict using the id as key
+        # Insert start nodes into the heap
+        self.heap.append((self.start, -999))       # Note: init the root node's parent to -999 to indicate null
+        distances[self.start] = 0
 
-            # Check if goal is reached
+        # Continue until the heap is empty
+        while self.heap:
+            self.heap.sort(reverse=True, key=lambda x: distances[x[0]]) # sort the list to emulate a min-heap
+            curNode = self.heap.pop()
+            closedDict[curNode[0]] = curNode    # Insert current node to the closedDict
+
+            print("\tDEBUG: curNode = {}".format(curNode))
+
             if self.goal == curNode[0]:
-                self.build_shortest_path(curNode, closedDict)
-                break
-            
+                goalNode = curNode
+
             for edge in self.grab_edges(curNode):
-                adjNodeId = edge[1]                   # Grab id of the destination/adj node
-                edgeCost = edge[2]
-                newCost = edgeCost + pathCost
+                adjNodeId = edge[1]
+                newCost = distances[curNode[0]] + edge[2]
 
-                print("\tDEBUG: {}'s child node -- {}".format(curNode, adjNodeId))
+                print("\t\tDEBUG: curNode's children = {}".format(adjNodeId))
 
-                adjNodeContainer = [x for x in heap if x[0] == adjNodeId]
+                if (adjNodeId not in closedDict) or (newCost < distances[adjNodeId]):
+                    distances[adjNodeId] = newCost
 
-                print("\tDEBUG: adjNodeContainer = {}".format(adjNodeContainer))
+                    # Insert into self.heap if it doesn't exist
+                    # Update value if it already does
+                    adjNode = (adjNodeId, curNode[0])
+                    self.decrease_priority(adjNode)
+                        
 
-                adjNodePathCost = adjNodeContainer[0][1] if (len(adjNodeContainer) > 0) else math.inf
+            print("\t\tDEBUG: curNode = {} and self.heap contents = {}".format(curNode, [ (x, distances[x[0]]) for x in self.heap ]))
+            print("\t\t\tDEBUG: closedDict = {}".format(closedDict))
+        ###end-while
+
+            
+        self.build_shortest_path(goalNode, closedDict)
+        
+    ###end-find_shortest_path
 
 
 
-                print("\tDEBUG: adjNodePathCost = {}".format(adjNodePathCost))
+    def decrease_priority(self, node):
+        '''
+        This function checks if the current adjacent node exists in the heap.
+            - If it already exists, it updates the entry (so that the parent id is correct)
+            - Else, it adds it to the heap
+        '''
 
+        updated = False
+        for i,n in enumerate(self.heap):
+            if n[0] == node[0]:
+                self.heap[i] = node
+                updated = True
+                break
 
-                
-                boolB = newCost < adjNodePathCost
+        if updated == False:
+            self.heap.append(node)
+    ###end-decrease_priority
 
-                if adjNodeId not in closedDict:
-                    print("\t\t\tDEBUG: boolA = {} and boolB = {}".format(boolA, boolB))
-
-                    # If the currenct adjNode does not exist in the heap (i.e. fringe), add it 
-                    # Else, update the pathCost
-                    if len(adjNodeContainer) == 0:
-                        node = (newCost, adjNodeId, curNode[1]) # (pathCost, nodeId, parentId)
-                        # heapq.heappush(heap, node)
-                        heap.append(node)
-                    else:
-                        for i, node in enumerate(heap):
-                            if node[1] == adjNodeId:
-                                heap[i] = (newCost, node[1], node[2])   # replace the tuple
-                                # heapq.heapify(heap)                           # update the heap
-                                heap.sort(reverse=True, key=lambda x: x[1])
-
-            print("\t\tDEBUG: current contents of heap -- {}".format(heap))
-
-        # Return solution
-        self.shortestPath 
-
-    ###end-shortest_path
 
     def build_shortest_path(self, goalNode, closedDict):
         '''
             This method backtracks from the goal node to the root node by
             iterating over the closed list and builds the shortest path and
-            populates the data member shortestPath
+            populates the data member self.shortestPath
         '''
         solution = []
     
         curNode = goalNode
+
+
+        print("\n\nStarting build_shortest_path")
+
+
         # Iterate over closedDict following each node's parent and populate the solution list until the root is reached
         while True:
-            parentId = curNode[2]
-            childId = curNode[1]
+
+
+            print("\t\tDEBUG: curNode = {} and solution = {}".format(curNode, solution))
+
+
+            parentId = curNode[1]
 
             # Root reached
-            if parentId == -99:
+            if parentId == -999:
+                print("Breaking inside parent checker")
                 break
 
             for edge in self.table:
-                if edge[0] == parentId and edge[1] == childId:
+                if edge[0] == parentId and edge[1] == curNode[0]:
                     solution.append(edge)
                     break
+
             try:
-                curNode = [x for x in closedDict if x[1] == parentId][0]
+                curNode = [closedDict[x] for x in closedDict if closedDict[x][0] == parentId][0]
             except:
                 print("ERROR(1): curNode not found in closedDict ")
                 print(solution)
                 solution = None
                 break
+        ###end-while
             
         if solution:
             self.shortestPath = solution
         else:
-            self.shortestPath = None
-            print("ERROR(2): function failed")
+            self.shortestPath = []
+            print("ERROR(2): function build_shortest_path failed")
+
     ###end-build_shortest_path
 
-
+    def build_vertices_list(self):
+        nodes = []
+        for edge in self.table:
+            if edge[0] not in nodes:
+                nodes.append(edge[0])
+            if edge[1] not in nodes:
+                nodes.append(edge[1])
+        return nodes
+    ###end-function
 
     def grab_edges(self, node):
         # Note: 
             # edge[0] is the id of the source node
             # node[1] is the id of the current node
         # Return all edges that go out from the current node
-        return [edge for edge in self.table if edge[0] == node[1]]
+        return [edge for edge in self.table if edge[0] == node[0]]
     ###end-grab_edges
 
 
             
 
     def print_shortest_path(self):
-        for edge in self.shortestPath:
+        for edge in reversed(self.shortestPath):
             print("Vertex {} to vertex {} (edge weight of {})".format(edge[0], edge[1], edge[2]))
     ###end-print-path
 ###end-class
-
-
-
-
 
 
 def main():
